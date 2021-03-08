24d4564077e61e5b069a80babd9a7ae2ac94e989
haohai <luruiqian@gomeplus.com>
2021-03-08 17:12:08 +0800

    测试字母n的个数

--- src/main/java/StringDemo.java
+++ src/main/java/StringDemo.java
@@ -3,12 +3,12 @@ import java.util.regex.Pattern;
 
 public class StringDemo {
     public static void main(String[] args) {
-        System.out.println("java Test demo  new");
+        System.out.println("java Test demo new");
         //指定字符串
         String str = "welcome to china";
         //查找字符  o  出现的所有位置
         int num = 0;
-        Pattern pattern = Pattern.compile("o");
+        Pattern pattern = Pattern.compile("n");
         Matcher findMatcher = pattern.matcher(str);
 
             while(findMatcher.find()) {
